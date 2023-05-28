41529df3cc3fe78c172a8ab185b70154625b4c5e
Jaypa92 <96949038+Jaypa92@users.noreply.github.com>
2023-05-28 10:48:53 -0400

    Delete Project directory

--- Project/src/main/java/com/justin/Project/models/BreedComparator.java
+++
@@ -1,9 +0,0 @@
-package com.justin.Project.models;
-
-import java.util.Comparator;
-
-public class BreedComparator implements Comparator<Pets>{
-	public int compare(Pets p1, Pets p2) {
-		return p1.getBreed().compareTo(p2.getBreed());
-	}
-}
--- Project/src/main/java/com/justin/Project/models/TypeComparator.java
+++
@@ -1,9 +0,0 @@
-package com.justin.Project.models;
-
-import java.util.Comparator;
-
-public class TypeComparator implements Comparator<Pets> {
-	public int compare(Pets p1, Pets p2) {
-		return p1.getType().compareTo(p2.getType());
-	}
-}
